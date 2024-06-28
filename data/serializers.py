7bcea5dc3e2b22e60567823e8d41ada6f64b8d93
96587413 <le.zhang@parisinfrastructureadvisory.com>
2024-06-28 16:33:15 +0200

    Update serializers.py

--- data/serializers.py
+++ data/serializers.py
@@ -4,16 +4,84 @@ from .models import CustomUser,Organization,Contract,Exercise,File,Comment,Share
 from .models import FileAccess,MailBell
 from .models import OrgConRight,OrgExerRight,UserConRight,UserExerRight
 
+class ShareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Share
+        fields = '__all__'
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['from_user','to_user','date','file'].read_only =True
+class MailBellSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MailBell
+        fields = '__all__'
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['user'].read_only =True
+class FileAccessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FileAccess
+        fields = '__all__'
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['file'].read_only =True
+
+class OrgConRightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrgConRight
+        fields = '__all__'
+        read_only_fields = ['org','con','nb_acct']
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['org','con'].read_only =True
+class UserConRightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserConRight
+        fields = '__all__'
+        read_only_fields = ['user','con']
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['user','con'].read_only =True
+class OrgExerRightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrgExerRight
+        fields = '__all__'
+        read_only_fields = ['org','exer']
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['org','exer'].read_only =True
+class UserExerRightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserExerRight
+        fields = '__all__'
+        read_only_fields = ['user','exer']
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['user','exer'].read_only =True
+
+
 class UserSerializer(serializers.ModelSerializer):
+    mailbell = MailBellSerializer
     class Meta:
         model = CustomUser
         fields = '__all__'
-        read_only_fields = ['name', 'id']
+        read_only_fields = ['id']
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['org'].read_only =True
 class OrgSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
         fields = '__all__'
-        read_only_fields = ['name', 'id']
+        read_only_fields = ['id']
 class ConSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
@@ -24,50 +92,51 @@ class ExerSerializer(serializers.ModelSerializer):
         model = Exercise
         fields = '__all__'
         read_only_fields = ['id',]
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['con','org',].read_only =True
 class FileSerializer(serializers.ModelSerializer):
+    access = FileAccessSerializer(read_only=True)
     class Meta:
         model = File
         fields = '__all__'
-        read_only_fields = ['id','uploader']
+        read_only_fields = ['id']
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['uploader','con','exer'].read_only =True
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ['id','line','colone','commenter','parent','file']
-class ShareSerializer(serializers.ModelSerializer):
+        read_only_fields = ['id']
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        if self.instance is not None:
+            self.fields['commenter','parent','file'].read_only =True
+
+# Spaces
+class MySpaceShareSerializer(serializers.ModelSerializer):
     class Meta:
         model = Share
-        fields = '__all__'
-        read_only_fields = '__all__'
+        fields = ['file']
 
-class MailBellSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MailBell
-        fields = '__all__'
-        read_only_fields = ['user',]
-class FileAccessSerializer(serializers.ModelSerializer):
+class MySpaceAllSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileAccess
-        fields = '__all__'
-        read_only_fields = ['file',]
+        fields = ['file']
 
-class OrgConRightSerializer(serializers.ModelSerializer):
+class OrgSpaceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = OrgConRight
-        fields = '__all__'
-        read_only_fields = ['org','con','nb_acct']
-class UserConRightSerializer(serializers.ModelSerializer):
+        model = FileAccess
+        fields = ['file']
+
+class PublicSpaceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserConRight
-        fields = '__all__'
-        read_only_fields = ['user','con']
-class OrgExerRightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrgExerRight
-        fields = '__all__'
-        read_only_fields = ['org','exer']
-class UserExerRightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserExerRight
-        fields = '__all__'
-        read_only_fields = ['user','exer']
+        model = FileAccess
+        fields = ['file']
+# class TestSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Organization
+#         fields = ['id']
